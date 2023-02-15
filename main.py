a8d3fa018b95b48de05b22e2474d9a1c849d06d9
daniya_kuspanova <kyspanova@icloud.com>
2023-02-15 23:44:16 +0400

    problem with text encoding has been solved

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 alphabet_eu = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alphabet_ru = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+alphabet_ru = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
 
-def encryption(step: int, message: str, lang: int) -> None:
+def encryption(step: int, message: str, lang: str) -> None:
     f = open("source_text1", mode='w', encoding='utf-8')
     f.write(message)
     f.close()
     result = ''
-    if lang == 1:
+    if lang == '1':
         for i in message:
             place = alphabet_ru.find(i)
             new_place = place + step
@@ -31,6 +31,6 @@ def encryption(step: int, message: str, lang: int) -> None:
 if __name__ == '__main__':
     step = int(input('Шаг шифровки: '))
     message = input("Сообщение для шифровки: ").upper()
-    print('Выберите язык : 1) RU  2) EU\n')
+    print('Выберите язык : 1) RU  2) EU')
     lang = input()
     encryption(step, message, lang)
