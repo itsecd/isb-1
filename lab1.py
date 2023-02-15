1c882bb459ce588668101974c857320f12163821
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2023-02-15 20:48:41 +0400

    createKey function

--- lab1.py
+++ lab1.py
@@ -2,3 +2,17 @@
 def saveFile(str: str, fileName: str) -> None:
     with open(f'{fileName}.txt', 'w') as f:
         print(str, file=f)
+
+
+def createKey(n: int) -> str:
+    alph = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    alph = list(alph)
+    n = -n
+    if n < 0:
+        n = abs(n)
+        for i in range(n):
+            alph.append(alph.pop(0))
+    else:
+        for i in range(n):
+            alph.insert(0, alph.pop())
+    return alph
