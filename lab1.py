42a90166c6a0179071190daa89a39957de7f5f8b
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2023-02-15 20:48:59 +0400

    encrypt function

--- lab1.py
+++ lab1.py
@@ -16,3 +16,18 @@ def createKey(n: int) -> str:
         for i in range(n):
             alph.insert(0, alph.pop())
     return alph
+
+
+def encrypt(str: str, n: int):
+    alph = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    alph = alph.lower()
+    ln = len(alph)
+    res = []
+    noAlph = '.\/[]{}()=-.,;:\'"1234567890~!`@#$%^&*<>?|=+_- '
+    for l in str:
+        if not l in noAlph:
+            print(l)
+            res.append(alph[(alph.find(l)+n) % ln])
+        else:
+            res.append(l)
+    return res
