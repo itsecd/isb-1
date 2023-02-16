96e34e06eb5a4f005562cf36c278ad6857b44f21
K0tlyarski <djgame2.9.6@gmail.com>
2023-02-16 09:17:58 +0400

    making a dictionary generator. c1 is the key, c2 is the value

--- task1.py
+++ task1.py
@@ -4,6 +4,7 @@ from random import shuffle
 def generate_notepad() -> dict:
     alphabet = "йцукенгшщзхъфывапролджэячсмитьбю" + "йцукенгшщзхъфывапролджэячсмитьбю".upper()
     alphabet_cpy = shuffle_string(alphabet)
+    return {c1: c2 for c1,c2 in zip(alphabet, alphabet_cpy) }
 
 
 def shuffle_string(txt: str) -> str:
