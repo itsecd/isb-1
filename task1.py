410dcae6f8b1574b55e65121a26ea660aa38c5b2
K0tlyarski <djgame2.9.6@gmail.com>
2023-02-16 09:14:48 +0400

    creating a function that shuffles the alphabet

--- task1.py
+++ task1.py
@@ -1,4 +1,12 @@
 from random import shuffle
+
+
 def generate_notepad() -> dict:
     alphabet = "йцукенгшщзхъфывапролджэячсмитьбю" + "йцукенгшщзхъфывапролджэячсмитьбю".upper()
     alphabet_cpy = shuffle_string(alphabet)
+
+
+def shuffle_string(txt: str) -> str:
+    txt_lst = list(txt)
+    shuffle(txt_lst)
+    return ''.join(txt_lst)
