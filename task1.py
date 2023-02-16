9939ddf1366f36d4eb8000d980ce0de90de03c5e
K0tlyarski <djgame2.9.6@gmail.com>
2023-02-16 09:52:24 +0400

    implemented reading and writing to a file

--- task1.py
+++ task1.py
@@ -11,3 +11,23 @@ def shuffle_string(txt: str) -> str:
     txt_lst = list(txt)
     shuffle(txt_lst)
     return ''.join(txt_lst)
+
+
+def encrypt(text: str, notepad: dict = None) -> str:
+    if notepad is None:
+        notepad = generate_notepad()
+        print('Notepad is: ', notepad, sep='\n')
+
+    return ''.join([notepad.get(c, c) for c in text])
+
+
+if __name__ == "__main__":
+    with open("task1_text") as file:
+        s = file.read()
+    #print(encrypt(s))
+    #print(s)
+    file_write = open("task1_encrypt.txt", "w")
+    file_write.write(encrypt(s))
+    file_write.close()
+
+
