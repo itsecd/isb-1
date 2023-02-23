9f74c5b7cfc2137840b1ebca1d544cab71bd7166
Darya Sakharova <daryasheqer@yandex.ru>
2023-02-24 01:20:01 +0400

    Added the function to get the key for the first task

--- task1.py
+++ task1.py
@@ -1,7 +1,31 @@
+symbols = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ .,-'
+
 def readFile (fileName: str) -> str:
+    '''Reading data from a file'''
     with open(f'{fileName}.txt', "r", encoding='utf-8') as file:
         return file.read()
 
-def savefile (message: str, fileName: str) -> None:
+def savefile (text: str, fileName: str) -> None:
+    '''Writing data to a file'''
     with open(f'{fileName}.txt', "w", encoding='utf-8') as file:
-        file.write(message)
+        file.write(text)
+
+def key1() -> dict:
+    '''Creating a key1 by shifting the original symbols and saving it to a file'''
+    #char_list = list(text.upper())
+    key ={}
+    for elem in symbols:
+        position = symbols.find(elem)
+        if position < 34:
+            key[symbols[position]] = symbols[position+3]
+        else:
+            key[symbols[position]] = symbols[position-34]
+    print(key)
+    with open('key1.txt', 'w', encoding='utf-8') as file:
+        file.write(str(key))
+    return key
+    
+
+if __name__ == '__main__':
+    key1()
+    
