bbb1d7c0c8ce74c0f8369fc8293846d8e5a5fb4b
Santas7 <andreypanyshkin03@gmail.com>
2023-02-23 11:28:35 +0400

    added a function to write data to the file

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ RUSSIAN_ALPHABET = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬ
 BY_FREQUENCY_ALPHABET = ' ОИЕАНТСРВМЛДЯКПЗЫЬУЧЖГХФЙЮБЦШЩЭЪ'
 
 def read_file(filename: str):
+    """
+        function to read data from a file
+    """
     with open(filename, 'r', encoding='utf-8', newline='') as file:
         return file.read().upper()
 
-"""def write_in_file(filename: str):
+def write_in_file(filename: str, data: str):
+    """
+        function to write data to file
+    """
     with open(filename, 'w', encoding='utf-8', newline='') as file:
-        print(file)"""
+        file.write(data)
 
 def text_encoding_сaesars_cipher():
     """
@@ -27,6 +33,7 @@ def text_encoding_сaesars_cipher():
             result_ciphers += i
     return result_ciphers
 
-text_encoding_сaesars_cipher()
+res = text_encoding_сaesars_cipher()
+write_in_file("encryption.txt", res)
 
 
