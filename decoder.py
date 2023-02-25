540fbfda62c39c3bd0e4e4c72bed22232892c5e7
pavel <tomashaytis@gmail.com>
2023-02-25 15:44:58 +0400

    Sorted word_frequencies by values. Created  the function read_true_frequencies(), which reads frequencies of russian alphabet from the file true_frequencies.txt. Moved to new function create_alphabet() getting the alphabet of ciphertext.

--- decoder.py
+++ decoder.py
@@ -2,6 +2,23 @@ from encryptor import read_text
 ALPHABET = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ '
 
 
+def read_true_frequencies(file_name: str) -> dict:
+    with open(file_name, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    true_frequencies = {}
+    for line in lines:
+        key, value = line.split()
+        true_frequencies[key] = value
+    return true_frequencies
+
+
+def create_alphabet(text: str):
+    alphabet = list(set(text))
+    alphabet.remove('\n')
+    alphabet = "".join(alphabet)
+    return alphabet
+
+
 def frequency_analysis(alphabet: str, text: str) -> dict:
     frequencies = [0] * len(alphabet)
     for word in text:
@@ -11,13 +28,17 @@ def frequency_analysis(alphabet: str, text: str) -> dict:
     for i in range(len(frequencies)):
         frequencies[i] /= word_count
     word_frequencies = dict(zip(alphabet, frequencies))
-    print(word_frequencies)
-    return word_frequencies
+    sorted_word_frequencies = {}
+    sorted_keys = sorted(word_frequencies, key=word_frequencies.get)
+    sorted_keys = sorted_keys[::-1]
+    for key in sorted_keys:
+        sorted_word_frequencies[key] = word_frequencies[key]
+    print(sorted_word_frequencies)
+    return sorted_word_frequencies
 
 
 if __name__ == "__main__":
     ciphertext = read_text('cod7.txt')
-    text_alphabet = list(set(ciphertext))
-    text_alphabet.remove('\n')
-    text_alphabet = "".join(text_alphabet)
+    text_alphabet = create_alphabet(ciphertext)
+    print(read_true_frequencies('true_frequencies.txt'))
     frequency_analysis(text_alphabet, ciphertext)
