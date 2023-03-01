7c0f329cb866838b2590b4b37b654b1d91b2572b
Alexey Zemel <loyxa.zemel@gmail.com>
2023-03-01 18:14:44 +0400

    add save text function and created key

--- first_task.py
+++ first_task.py
@@ -4,7 +4,11 @@ def create_key(alp: str, offset: int) -> str:
         alp.append(alp.pop(0))
     return ''.join(alp)
 
+def save_text(text:str, filename:str) -> None:
+    with open(f'{filename}.txt', 'w', encoding='utf-8') as f:
+        f.write(text)
 
 if __name__ == '__main__':
     alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-    key = create_key(alphabet)
+    key = create_key(alphabet, 5)
+    save_text(key, 'key1')
