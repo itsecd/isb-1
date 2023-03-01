de3f927d7abc00496cfc82a5b801c8a7148f3a73
Alexey Zemel <loyxa.zemel@gmail.com>
2023-03-02 01:32:16 +0400

    added key for second task and code got prettier

--- first_task.py
+++ first_task.py
@@ -1,36 +1,40 @@
 def create_key(alp: str, offset: int) -> str:
     alp = list(alp)
-    for i in range (abs(offset)):
+    for i in range(abs(offset)):
         alp.append(alp.pop(0))
-    return ''.join(alp)
+    return "".join(alp)
 
-def save_text(text:str, filename:str) -> None:
-    with open(f'{filename}.txt', 'w', encoding='utf-8') as f:
+
+def save_text(text: str, filename: str) -> None:
+    with open(f"{filename}.txt", "w", encoding="utf-8") as f:
         f.write(text)
+
 
 def encryption(text: str, alp: str, key: str) -> str:
     offset = 0
-    symbols = '1234567890!@#$%^&*()?\/[]{}()=-.,; :\'"'
+    symbols = "1234567890!@#$%^&*()?\/[]{}()=-.,; :'\""
     key = list(key)
     for letter in list(alp):
-        if letter == key[0]: break
-        offset+=1
+        if letter == key[0]:
+            break
+        offset += 1
     result = []
     alp = alp.lower()
     text = text.lower()
     length = len(alp)
     for letter in text:
         if not letter in symbols:
-            result.append(alp[(alp.find(letter)+offset) % length])
+            result.append(alp[(alp.find(letter) + offset) % length])
         else:
             result.append(letter)
-    return ''.join(result)
+    return "".join(result)
 
-if __name__ == '__main__':
-    alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+
+if __name__ == "__main__":
+    alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
     key = create_key(alphabet, 5)
-    save_text(key, 'key1')
-    with open('text_first.txt', 'r', encoding = 'utf-8') as f:
+    save_text(key, "key1")
+    with open("text_first.txt", "r", encoding="utf-8") as f:
         topic = f.read()
     encrypted_text = encryption(topic, alphabet, key)
-    save_text(encrypted_text, 'encrypted_text_first')
+    save_text(encrypted_text, "encrypted_text_first")
--- second_task.py
+++ second_task.py
@@ -1,16 +1,18 @@
 from first_task import save_text
 
-def get_alp_freq(filename:str) -> dict:
+
+def get_alp_freq(filename: str) -> dict:
     result = {}
-    with open(f'{filename}.txt', 'r', encoding = 'utf-8') as f:
+    with open(f"{filename}.txt", "r", encoding="utf-8") as f:
         for i in range(33):
             row = f.readline()
-            row = row.split('\n')
-            row = row[0].split('=')
+            row = row.split("\n")
+            row = row[0].split("=")
             result[row[0]] = [row[1]]
     return result
 
-def get_text_freq(text:str) -> dict:
+
+def get_text_freq(text: str) -> dict:
     result = {}
     length = len(text)
     text_letters = []
@@ -22,21 +24,22 @@ def get_text_freq(text:str) -> dict:
     for letter in text_letters:
         result[letter] = text.count(letter) / length
 
-    result = sorted(result.items(), key = lambda x: x[1])
+    result = sorted(result.items(), key=lambda x: x[1])
     result = dict(result[::-1])
 
-    with open('text_freq.txt', 'w', encoding = 'utf-8') as f:
+    with open("text_freq.txt", "w", encoding="utf-8") as f:
         let = list(result.keys())
         fre = list(result.values())
         for i in range(32):
-            row = let[i] + '=' + str(fre[i]) + '\n'
+            row = let[i] + "=" + str(fre[i]) + "\n"
             f.writelines(row)
 
     return result
 
-if __name__ == '__main__':
-    freq_letters = get_alp_freq('frequency')
-    with open('encr_cod4.txt', 'r', encoding='utf-8') as f:
+
+if __name__ == "__main__":
+    freq_letters = get_alp_freq("frequency")
+    with open("encr_cod4.txt", "r", encoding="utf-8") as f:
         topic = f.read()
     text_freq = get_text_freq(topic)
 
@@ -45,37 +48,75 @@ if __name__ == '__main__':
 
     decrypted_text = topic
 
-    decrypted_text = decrypted_text.replace(text_freq[31][0], freq_letters[29][0])
-    decrypted_text = decrypted_text.replace(text_freq[17][0], freq_letters[17][0])
-    decrypted_text = decrypted_text.replace(text_freq[0][0], freq_letters[0][0])
-    decrypted_text = decrypted_text.replace(text_freq[14][0], freq_letters[10][0])
-    decrypted_text = decrypted_text.replace(text_freq[27][0], freq_letters[22][0])
-    decrypted_text = decrypted_text.replace(text_freq[29][0], freq_letters[30][0])
-    decrypted_text = decrypted_text.replace(text_freq[18][0], freq_letters[18][0])
-    decrypted_text = decrypted_text.replace(text_freq[15][0], freq_letters[13][0])
-    decrypted_text = decrypted_text.replace(text_freq[22][0], freq_letters[24][0])
-    decrypted_text = decrypted_text.replace(text_freq[3][0], freq_letters[7][0])
-    decrypted_text = decrypted_text.replace(text_freq[1][0], freq_letters[2][0])
-    decrypted_text = decrypted_text.replace(text_freq[2][0], freq_letters[3][0])
-    decrypted_text = decrypted_text.replace(text_freq[4][0], freq_letters[1][0])
-    decrypted_text = decrypted_text.replace(text_freq[5][0], freq_letters[6][0])
-    decrypted_text = decrypted_text.replace(text_freq[6][0], freq_letters[5][0])
-    decrypted_text = decrypted_text.replace(text_freq[7][0], freq_letters[4][0])
-    decrypted_text = decrypted_text.replace(text_freq[20][0], freq_letters[16][0])
-    decrypted_text = decrypted_text.replace(text_freq[8][0], freq_letters[8][0])
-    decrypted_text = decrypted_text.replace(text_freq[9][0], freq_letters[9][0])
-    decrypted_text = decrypted_text.replace(text_freq[10][0], freq_letters[12][0])
-    decrypted_text = decrypted_text.replace(text_freq[16][0], freq_letters[21][0])
-    decrypted_text = decrypted_text.replace(text_freq[11][0], freq_letters[15][0])
-    decrypted_text = decrypted_text.replace(text_freq[26][0], freq_letters[26][0])
-    decrypted_text = decrypted_text.replace(text_freq[12][0], freq_letters[14][0])
-    decrypted_text = decrypted_text.replace(text_freq[13][0], freq_letters[11][0])
-    decrypted_text = decrypted_text.replace(text_freq[24][0], freq_letters[23][0])
-    decrypted_text = decrypted_text.replace(text_freq[19][0], freq_letters[25][0])
-    decrypted_text = decrypted_text.replace(text_freq[21][0], freq_letters[19][0])
-    decrypted_text = decrypted_text.replace(text_freq[28][0], freq_letters[31][0])
-    decrypted_text = decrypted_text.replace(text_freq[23][0], freq_letters[27][0])
-    decrypted_text = decrypted_text.replace(text_freq[30][0], freq_letters[28][0])
-    decrypted_text = decrypted_text.replace(text_freq[25][0], freq_letters[20][0])
+    key = {}
 
-    save_text(decrypted_text, 'decrypted_cod4')
+    decrypted_text = decrypted_text.replace(text_freq[31][0], freq_letters[29][0])
+    key[text_freq[31][0]] = freq_letters[29][0]
+    decrypted_text = decrypted_text.replace(text_freq[17][0], freq_letters[17][0])
+    key[text_freq[17][0]] = freq_letters[17][0]
+    decrypted_text = decrypted_text.replace(text_freq[0][0], freq_letters[0][0])
+    key[text_freq[0][0]] = freq_letters[0][0]
+    decrypted_text = decrypted_text.replace(text_freq[14][0], freq_letters[10][0])
+    key[text_freq[14][0]] = freq_letters[10][0]
+    decrypted_text = decrypted_text.replace(text_freq[27][0], freq_letters[22][0])
+    key[text_freq[27][0]] = freq_letters[22][0]
+    decrypted_text = decrypted_text.replace(text_freq[29][0], freq_letters[30][0])
+    key[text_freq[29][0]] = freq_letters[30][0]
+    decrypted_text = decrypted_text.replace(text_freq[18][0], freq_letters[18][0])
+    key[text_freq[18][0]] = freq_letters[18][0]
+    decrypted_text = decrypted_text.replace(text_freq[15][0], freq_letters[13][0])
+    key[text_freq[15][0]] = freq_letters[13][0]
+    decrypted_text = decrypted_text.replace(text_freq[22][0], freq_letters[24][0])
+    key[text_freq[22][0]] = freq_letters[24][0]
+    decrypted_text = decrypted_text.replace(text_freq[3][0], freq_letters[7][0])
+    key[text_freq[3][0]] = freq_letters[7][0]
+    decrypted_text = decrypted_text.replace(text_freq[1][0], freq_letters[2][0])
+    key[text_freq[1][0]] = freq_letters[2][0]
+    decrypted_text = decrypted_text.replace(text_freq[2][0], freq_letters[3][0])
+    key[text_freq[2][0]] = freq_letters[3][0]
+    decrypted_text = decrypted_text.replace(text_freq[4][0], freq_letters[1][0])
+    key[text_freq[4][0]] = freq_letters[1][0]
+    decrypted_text = decrypted_text.replace(text_freq[5][0], freq_letters[6][0])
+    key[text_freq[5][0]] = freq_letters[6][0]
+    decrypted_text = decrypted_text.replace(text_freq[6][0], freq_letters[5][0])
+    key[text_freq[6][0]] = freq_letters[5][0]
+    decrypted_text = decrypted_text.replace(text_freq[7][0], freq_letters[4][0])
+    key[text_freq[7][0]] = freq_letters[4][0]
+    decrypted_text = decrypted_text.replace(text_freq[20][0], freq_letters[16][0])
+    key[text_freq[20][0]] = freq_letters[16][0]
+    decrypted_text = decrypted_text.replace(text_freq[8][0], freq_letters[8][0])
+    key[text_freq[8][0]] = freq_letters[8][0]
+    decrypted_text = decrypted_text.replace(text_freq[9][0], freq_letters[9][0])
+    key[text_freq[9][0]] = freq_letters[9][0]
+    decrypted_text = decrypted_text.replace(text_freq[10][0], freq_letters[12][0])
+    key[text_freq[10][0]] = freq_letters[12][0]
+    decrypted_text = decrypted_text.replace(text_freq[16][0], freq_letters[21][0])
+    key[text_freq[16][0]] = freq_letters[21][0]
+    decrypted_text = decrypted_text.replace(text_freq[11][0], freq_letters[15][0])
+    key[text_freq[11][0]] = freq_letters[15][0]
+    decrypted_text = decrypted_text.replace(text_freq[26][0], freq_letters[26][0])
+    key[text_freq[26][0]] = freq_letters[26][0]
+    decrypted_text = decrypted_text.replace(text_freq[12][0], freq_letters[14][0])
+    key[text_freq[12][0]] = freq_letters[14][0]
+    decrypted_text = decrypted_text.replace(text_freq[13][0], freq_letters[11][0])
+    key[text_freq[13][0]] = freq_letters[11][0]
+    decrypted_text = decrypted_text.replace(text_freq[24][0], freq_letters[23][0])
+    key[text_freq[24][0]] = freq_letters[23][0]
+    decrypted_text = decrypted_text.replace(text_freq[19][0], freq_letters[25][0])
+    key[text_freq[19][0]] = freq_letters[25][0]
+    decrypted_text = decrypted_text.replace(text_freq[21][0], freq_letters[19][0])
+    key[text_freq[21][0]] = freq_letters[19][0]
+    decrypted_text = decrypted_text.replace(text_freq[28][0], freq_letters[31][0])
+    key[text_freq[28][0]] = freq_letters[31][0]
+    decrypted_text = decrypted_text.replace(text_freq[23][0], freq_letters[27][0])
+    key[text_freq[23][0]] = freq_letters[27][0]
+    decrypted_text = decrypted_text.replace(text_freq[30][0], freq_letters[28][0])
+    key[text_freq[30][0]] = freq_letters[28][0]
+    decrypted_text = decrypted_text.replace(text_freq[25][0], freq_letters[20][0])
+    key[text_freq[25][0]] = freq_letters[20][0]
+
+    save_text(decrypted_text, "decrypted_cod4")
+
+    with open("key2.txt", "w", encoding="utf-8") as f:
+        for k, v in key.items():
+            f.write(f"{k} = {v}\n")
