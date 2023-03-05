6b2d1696a129ae8a11c01aff23e3b6baccb4c7c8
Миша Чукарев <mishakrut11@gmail.com>
2023-03-05 23:18:52 +0400

    write func. cipher

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-symbols='АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,- '
+
 
 def read_file(file_name):
-    with open(f'{file_name}.txt','r',encoding='utf-8') as file:
+    with open(f'{file_name}.txt', 'r', encoding='utf-8') as file:
         return file.read()
-    
+
+
 def save_file(file_name, text):
-    with open(f'{file_name}.txt', 'w',encoding='utf-8') as file:
+    with open(f'{file_name}.txt', 'w', encoding='utf-8') as file:
         file.write(text)
+
+
+def caesar_cipher(text, shift):
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    result = ''
+    for char in text:
+        if char.lower() in alphabet:
+            index = alphabet.index(char.lower())
+            shifted_index = (index + shift) % len(alphabet)
+            if char.isupper():
+                result += alphabet[shifted_index].upper()
+            else:
+                result += alphabet[shifted_index]
+        else:
+            result += char
+    return result
+
+
+def caesar_cipher(text, shift):
+    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    text = text.lower()
+    encrypted_text = ""
+    for char in text:
+        if char in alphabet:
+            index = alphabet.index(char)
+            shifted_index = (index + shift) % len(alphabet)
+            encrypted_text += alphabet[shifted_index]
+        else:
+            encrypted_text += char
+    return encrypted_text
